7f50cdd7b11787f9ab98866011c3da5291b77a57
Rohit-Borah <hie.rohitb@gmail.com>
2025-10-09 16:27:31 +0530

    Update

--- nhpapi_quickQ.sql
+++ nhpapi_quickQ.sql
@@ -1,2 +1,4 @@
 SELECT * FROM public.nhp_rtdas_ingest
-WHERE "DateTime"::date = '31/07/25' AND "StationID" IN ('&56067970','&5605C322','&5605E5CE')
+--WHERE "DateTime"::date BETWEEN '25/07/25' AND '02/08/25'
+WHERE "DateTime"::date >= '26/07/25' AND "StationID" IN ('&5604C1D8','&5604CF0A','&5604D2AE','&5604DC7C','&5604E9E6')
+ORDER BY "DateTime"::date ASC
